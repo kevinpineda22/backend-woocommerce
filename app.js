ab5879187991610ca323fd35c63123a29b3d3590
josanva777 <johansanchezvalencia@gmail.com>
2026-01-07 14:09:16 -0500

    cambio de url de local a produccion

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const orderRoutes = require('./routes/orderRoutes');
+const orderRoutes = require("./routes/orderRoutes");
 
 const app = express();
 
-app.use(cors()); // Permite que tu React se conecte sin errores de CORS
+// Esto permite que cualquier dominio (incluyendo tu web de React en producción)
+// consuma los datos de los pedidos.
+app.use(cors());
 app.use(express.json());
 
 // Rutas
-app.use('/api/orders', orderRoutes);
+app.use("/api/orders", orderRoutes);
 
 // Ruta de prueba para saber que el backend vive
-app.get('/', (req, res) => res.send('Backend del Supermercado Funcionando 🛒'));
+app.get("/", (req, res) => res.send("Backend del Supermercado Funcionando 🛒"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
